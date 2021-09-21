58071a0560f92474cdc35e59f9f3c123a8fb02ef
Maria Korogodova <MMKorogodova@sberbank.ru>
2021-09-21 11:43:48 +0300

    day00ex00

---
+++ day00/ex00/ex00/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by OUT-Korogodova-MM on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    @IBAction func Button(_ sender: UIButton) {
+        print("Зачем ты нажал?")
+    }
+}
+
