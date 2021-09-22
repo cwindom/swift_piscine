c4d17f376e4337e347883f6c76a51ef358de2db6
Maria Korogodova <MMKorogodova@sberbank.ru>
2021-09-22 16:15:20 +0300

    ex00

---
+++ day01/ex00/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  ex00
+//
+//  Created by OUT-Korogodova-MM on 22.09.2021.
+//
+
+import Foundation
+
+public enum Color: String {
+    
+    case hearts = "hearth"
+    case diamonds = "diamonds"
+    case clubs = "clubs"
+    case spades = "spades"
+    
+    static let allColors: [Color] = [hearts, diamonds, clubs, spades]
+}
---
+++ day01/ex00/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  ex00
+//
+//  Created by OUT-Korogodova-MM on 22.09.2021.
+//
+
+import Foundation
+
+var color = Color.allColors
+var value = Value.allValues
+
+for i in color {
+    print("\(i): \(i.rawValue)")
+}
+
+for i in value {
+    print("\(i): \(i.rawValue)")
+}
